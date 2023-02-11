e79a9dd80a60a21cf18dc62823a6d0eb12c54f59
mehdi657 <mehdi657@gmail.com>
2023-02-11 22:15:05 +0100

    mise en place des fichiers et dossiers et sass

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+// import { BrowserRouter as Router } from 'react-router-dom'
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      Bonjour !!!
+    </div>
+  );
+};
+
+export default Header;
