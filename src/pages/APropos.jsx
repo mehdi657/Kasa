578c762e0ee42e70ea7db79cc667d9958e724068
mehdi657 <mehdi657@gmail.com>
2023-02-20 16:44:07 +0100

    mise en place des "media queries"  et finalisation de toutes les pages

--- src/pages/APropos.jsx
+++ src/pages/APropos.jsx
@@ -6,13 +6,17 @@ import Volet from '../components/Volet'
 
 const APropos = () => {
   return (
-    <section className='about_section'>
-      <Banner image={image} alt={textes.altBanner2} text={textes.null} />
-      <Volet titre="Fiabilité" texte={textes.fiabilité} />
-      <Volet titre="Respect" texte={textes.respect} />
-      <Volet titre="Service" texte={textes.service} />
-      <Volet titre="Sécurité" texte={textes.sécurité} />
-    </section>
+    <>
+      <section className="about_banner">
+        <Banner image={image} alt={textes.altBanner2} text={textes.null} />
+      </section>
+      <section className="about_section">
+        <Volet titre="Fiabilité" texte={textes.fiabilité} />
+        <Volet titre="Respect" texte={textes.respect} />
+        <Volet titre="Service" texte={textes.service} />
+        <Volet titre="Sécurité" texte={textes.sécurité} />
+      </section>
+    </>
   )
 }
 
